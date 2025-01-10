fc3795d684e0a4c0ef187cb63528eb91be9b618f
ddaspodder <ddaspodder@gmail.com>
2025-01-11 01:11:17 +0530

    udpated counter

--- src/Counter.js
+++ src/Counter.js
@@ -7,7 +7,7 @@ function Counter() {
     <div>
       <h1>Counter</h1>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 2)}>Click me</button>
+      <button onClick={() => setCount(count + 3)}>Click me</button>
     </div>
   );
 }
